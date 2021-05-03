08b1f829028faeaa6fa13d087b78d000d38741bc
Somesh <samchopz5@gmail.com>
2021-05-03 18:51:23 +0530

    Updated package json main script

--- server.js
+++ server.js
@@ -27,8 +27,7 @@ app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/', reportRouter)
-app.use('/start',(req,res,next)=>{
+app.use('/', (req,res,next)=>{
 	res.send("Successfull");
 })
 app.use('/report', reportRouter)
