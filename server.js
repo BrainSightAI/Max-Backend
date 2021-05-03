f24a4212812c4aaf0afa40068f0e1524ea6d2ab9
Somesh <samchopz5@gmail.com>
2021-05-03 20:38:23 +0530

    added login logic in server js file

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const reportRouter = require('./routes/reportRouter')
-const loginRouter = require('./routes/loginRouter')
 const registerRouter = require('./routes/registerRouter')
 const cors = require('cors')
 const bodyParser= require('body-parser');
@@ -31,7 +30,69 @@ app.use('/', (req,res,next)=>{
 	res.send("Successfull");
 })
 app.use('/report', reportRouter)
-app.use('/login', loginRouter)
+app.use('/login', (req, res) => {
+		res.send("Login Success");
+      /*const userId = req.body.userId
+      const password = req.body.password
+
+      User.findOne({userId:userId}).then(user=>{
+            if(!user){
+                  return res.status(401).json({message:"Cannot find a User with this Id"});
+            }
+            return bcrypt.compare(password,user.password);
+      }).then(result=>{
+            if(!result){
+                 return res.status(401).json({ message: "Invalid credentials, try again" });
+            }
+            const token=jwt.sign({userId:userId},process.env.JWT_SECRET_KEY);
+            res.status(200).json({
+                  accessToken:token
+            })
+      }).catch(()=>{
+            res.status(201).json({message:"Something went wrong!!"});
+      })*/
+
+      /*
+      const db = new AWS.DynamoDB.DocumentClient()
+      var params = {
+            TableName: process.env.PROD_USERS_TABLE_NAME,
+            KeyConditionExpression: "#userId = :enteredUserId",
+            ExpressionAttributeNames: {
+                  "#userId": "userId"
+            },
+            ExpressionAttributeValues: {
+                  ":enteredUserId": userId
+            }
+      }
+
+      if (!(password && userId)) {
+            return res.status(400).json({ message: "Bad request" })
+      }
+
+      db.query(params, async (err, data) => {
+            if (err) {
+                  console.error("Unable to query. Error:", JSON.stringify(err, null, 2))
+            } else {
+                  if (data.Count === 0) {
+                        return res.status(404).json({ message: `No user found with userId=${userId}` })
+                  }
+
+                  const item = data.Items[0]
+                  const user = {
+                        userId: item.userId
+                  }
+                  try {
+                        if (item.userId === userId && await bcrypt.compare(password, item.hashedPassword)) {
+                              res.status(200).json({ accessToken: jwt.sign(user, process.env.JWT_SECRET_KEY) })
+                        } else {
+                              res.status(401).json({ message: "Invalid credentials, try again" })
+                        }
+                  } catch {
+                        res.status(500).json({ message: "Internal server error" })
+                  }
+            }
+      })*/
+})
 app.use('/register', registerRouter)
 
 app.listen(process.env.PORT, () => {
